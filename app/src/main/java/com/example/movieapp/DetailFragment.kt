c0d94e279c481192bfd4fedd50e49ea752a58dde
Emanuel <gostner.emanuel@gmail.com>
2021-03-27 18:28:40 +0100

    add stuff for lab3

--- app/src/main/java/com/example/movieapp/DetailFragment.kt
+++ app/src/main/java/com/example/movieapp/DetailFragment.kt
@@ -5,11 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
+import com.example.movieapp.databinding.ActivityMainBinding
+import com.example.movieapp.databinding.FragmentDetailBinding
+import com.example.movieapp.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
+
+@BindingAdapter("android:text")
+fun setText(view: TextView, list: MutableList<String>) {
+    view.text = list.joinToString(", ")
+}
 
 /**
  * A simple [Fragment] subclass.
@@ -18,40 +29,25 @@ private const val ARG_PARAM2 = "param2"
  */
 class DetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        val binding = DataBindingUtil.inflate<FragmentDetailBinding>(inflater, R.layout.fragment_detail, container, false)
+
+        //bind click Handler
+        binding.fabButton.setOnClickListener {
+            clickHandlerFunction(it)
+        }
+
+        //bind object instance to layout
+        binding.movie = Movie();
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                DetailFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    private fun clickHandlerFunction(viewThatIsClicked: View) {
+       /* Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()*/
     }
 }
--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -8,12 +8,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.movieapp.databinding.ActivityMainBinding
-
-@BindingAdapter("android:text")
-fun setText(view: TextView, list: MutableList<String>) {
-    view.text = list.joinToString(", ")
-}
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,18 +20,11 @@ class MainActivity : AppCompatActivity() {
         val binding : ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        //bind click Handler
-        binding.fabButton.setOnClickListener {
-            clickHandlerFunction(it)
-        }
-
-        //bind object instance to layout
-        binding.movie = Movie();
-
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
     }
-
-    private fun clickHandlerFunction(viewThatIsClicked: View) {
-        Toast.makeText(this, "button clicked",
-                Toast.LENGTH_SHORT).show()
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
